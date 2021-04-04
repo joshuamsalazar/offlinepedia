b30192a03a41ff09cfa8297a0d5ab9f724c415ed
Joshua <36059512+joshuamsalazar@users.noreply.github.com>
2021-04-04 16:05:28 -0500

    Completed auto install script
    Test pending. Has to completely run first

--- install_kiwix_RaspberryPi.sh
+++ install_kiwix_RaspberryPi.sh
@@ -5,14 +5,16 @@
 echo "\n\t\t\t Please open it with administrator (sudo) privileges"
 echo ""
 echo "Creating folders and copying..."
-mkdir ~/bin/kiwix
+mkdir -p ~/bin/kiwix
 cd ~/bin/kiwix
 wget https://download.kiwix.org/release/kiwix-tools/kiwix-tools_linux-armhf-3.1.2.tar.gz
 tar xf kiwix-tools_linux-armhf-3.1.2.tar.gz
 cd kiwix-tools_linux-armhf-3.1.2
-cp -vfr kiwix-tools_linux-i586-3.1.2 zims $HOME/bin/kiwix
+wget https://download.kiwix.org/zim/vikidia/vikidia_es_all_maxi_2021-03.zim
+sudo ./kiwix-serve vikidia_es_all_maxi_2021-03.zim
+#cp -vfr kiwix-tools_linux-i586-3.1.2 zims $HOME/bin/kiwix
 
-chmod +x /etc/rc.local
+sudo chmod 777 /etc/rc.local
 echo "#Kiwix startup command" >> /etc/rc.local
 echo $HOME/bin/kiwix/kiwix-tools_linux-i586-3.1.2/kiwix-serve $HOME/bin/kiwix/zims/*.zim $HOME/bin/kiwix/zims/*.zimaa >> /etc/rc.local 
-sh /etc/rc.local
+#sh /etc/rc.local
