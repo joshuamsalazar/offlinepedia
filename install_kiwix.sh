08ddee7fb5676a3cfa6702eb40690cc7ecd2b161
Joshua <36059512+joshuamsalazar@users.noreply.github.com>
2021-04-04 15:48:11 -0500

    Desktop Linux differentiation

--- install_kiwix.sh
+++ install_kiwix.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Installer for Kiwix serve
+#Installer for Kiwix serve over Desktop linux
 #
 #Please copy any new zim files to the zims folder and they will be automatically updated
 echo "\n\t\t\t Please open it with administrator (sudo) privileges"
@@ -7,7 +7,7 @@ echo ""
 echo "Creating folders and copying..."
 mkdir ~/bin/kiwix
 cd ~/bin/kiwix
-wget https://download.kiwix.org/release/kiwix-tools/kiwix-tools_linux-armhf-3.1.2.tar.gz
+#wget https://download.kiwix.org/release/kiwix-tools/kiwix-tools_linux-armhf-3.1.2.tar.gz
 cp -vfr kiwix-tools_linux-i586-3.1.2 zims $HOME/bin/kiwix
 
 chmod +x /etc/rc.local
