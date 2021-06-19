f2c07c30e9f3bff97b98293e5b3969de2e7c6fab
Joshua <36059512+joshuamsalazar@users.noreply.github.com>
2021-06-19 09:02:08 -0500

    Updated auto install over /home/USER/bin/kiwix
    Testing required

--- install_kiwix.sh
+++ install_kiwix.sh
@@ -7,8 +7,19 @@ echo ""
 echo "Creating folders and copying..."
 mkdir ~/bin/kiwix
 cd ~/bin/kiwix
-#wget https://download.kiwix.org/release/kiwix-tools/kiwix-tools_linux-armhf-3.1.2.tar.gz
-cp -vfr kiwix-tools_linux-i586-3.1.2 zims $HOME/bin/kiwix
+wget https://download.kiwix.org/release/kiwix-tools/kiwix-tools_linux-i586-3.1.2.tar.gz
+tar xf kiwix-tools_linux-i586-3.1.2.tar.gz
+
+mkdir zims
+cd zims
+wget https://download.kiwix.org/zim/vikidia/vikidia_es_all_maxi_2021-03.zim
+cd ..
+#cp -vfr kiwix-tools_linux-i586-3.1.2 zims $HOME/bin/kiwix
+
+#Testear
+cd ~/bin/kiwix/kiwix-tools_linux-i586-3.1.2
+./kiwix-serve ../zims/*.zim ../zims/*.zimaa
+
 
 chmod +x /etc/rc.local
 echo "#Kiwix startup command" >> /etc/rc.local
