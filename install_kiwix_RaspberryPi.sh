2140964290d5bc38f5de0139c321dd9414d5400d
Joshua <36059512+joshuamsalazar@users.noreply.github.com>
2021-06-20 04:04:03 +0100

    Update install_kiwix_RaspberryPi.sh

--- install_kiwix_RaspberryPi.sh
+++ install_kiwix_RaspberryPi.sh
@@ -18,7 +18,7 @@ wget https://download.kiwix.org/zim/vikidia/vikidia_es_all_maxi_2021-03.zim
 
 sudo chmod 777 /etc/rc.local
 echo "#Kiwix startup command" >> /etc/rc.local
-echo $HOME/bin/kiwix/kiwix-tools_linux-armhf-3.1.2/kiwix-serve $HOME/bin/kiwix/zims/*.zim >> /etc/rc.local # $HOME/bin/kiwix/zims/*.zimaa >> /etc/rc.local 
+echo $HOME/bin/kiwix/kiwix-tools_linux-armhf-3.1.2/kiwix-serve $HOME/bin/kiwix/zims/\*.zim >> /etc/rc.local # $HOME/bin/kiwix/zims/*.zimaa >> /etc/rc.local 
 sudo sh /etc/rc.local
 
 read -p "\n\n\t\t Kiwix instalado correctamente: ¡Disfruta de tu OfflinePedia! \n\n Presiona ENTER para abrir el lector"
